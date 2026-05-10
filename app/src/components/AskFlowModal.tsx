b54058b0092362c0521d084273109a48386d4862
notebook da lesli <joao.eudes13jem@gmail.com>
2026-05-10 05:29:34 -0300

    feat: implement multi-step ask flow modal structure

---
+++ app/src/components/AskFlowModal.tsx
@@ -0,0 +1,68 @@
+import { StyleSheet, Modal, View, Text, Pressable } from "react-native"
+import { useState } from "react"
+
+import { ProfessionalCard } from "./ProfessionalCard.tsx"
+import { ResponseWay } from "./ResponseWay.tsx"
+
+
+type Props = {
+    isVisible: boolean
+    onClose: () => void
+}
+
+export const AskFlowModal = ({ isVisible, onClose }: Props) => {
+    const [step, setStep] = useState(1)
+
+    const handleClose = () => {
+        setStep(1)
+        onClose()
+    }
+
+    return (
+        <Modal visible={isVisible} transparent animationType="fade">
+            <View style={styles.overlay}>
+                <View style={styles.modalBox}>
+                    {step === 1 && (
+                        <>
+                            <ProfessionalCard
+                                nextStep={() => setStep(2)}
+                                handleClose={handleClose}
+                            />
+                        </>
+                    )}
+                    {step === 2 && (
+                        <>
+                            <ResponseWay
+                                nextStep={() => setStep(3)}
+                                handleClose={handleClose}
+                            />
+                        </>
+                    )}
+                    {step === 3 && (
+                      <>
+                        <Text>
+                          Sua resposta
+                        </Text>
+                      </>
+                    )}
+                </View>
+            </View>
+        </Modal>
+    )
+}
+
+const styles = StyleSheet.create({
+    overlay: {
+        flex: 1,
+        backgroundColor: "rgba(0, 0, 0, 0.5)",
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    modalBox: {
+        width: "70%",
+        backgroundColor: "#fff",
+        padding: 20,
+        borderRadius: 12,
+        alignItems: "center",
+    },
+})
